2189e93f8243ee1606b1a4887938f1cb70f2293c
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2023-11-05 23:30:58 -0500

    made final changes

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
-/* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector('.newsletter form');
+    const messageDiv = document.querySelector('.newsletter .message');
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault(); 
+        const emailInput = document.getElementById('email');
+        if(email.value) {
+            messageDiv.innerHTML = `Thank you! Your email address <strong>${email.value}</strong> has been added to our mailing list!`;
+            emailInput.value = '';
+        
+        }
+
+    });
+});
